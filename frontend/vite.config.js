f67d4c28ab664b77489c250adb4f0432fe84ee46
Teemu Karjalainen <teekar@gmail.com>
2026-09-19 23:31:55 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Korvaa 'projektin-nimi' GitHub-repositoriosi täydellisellä nimellä
-  base: 'github.com/DesmondX80/pokerikatko',
+  base: 'pokerikatko',
 })
