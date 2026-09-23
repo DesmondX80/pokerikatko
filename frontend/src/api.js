0b6ff00d730c200e1736d5fead1c1e728ce8aa81
Teemu Karjalainen <teekar@gmail.com>
2026-09-23 16:32:44 +0300

    Asetettu oikea BASE_URL osoite

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://pokerikatko-backend.<sinun-tunnuksesi>.workers.dev/api';
+https://pokerikatko-backend.desmond80x.workers.dev/api';
 const API_URL = 'https://pokerikatko-backend.d874123151f7438abcb219b827ccf08a.workers.dev';
 
 async function handle(response) {
